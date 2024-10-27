ba6348afd4f6dacdb57a0f6232347480c064d621
marcellanunees <00001073887236sp@al.educacao.sp.gov.br>
2024-10-27 00:33:09 -0300

    fleshcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Filmes',
+    'Qual foi o primeiro filme animado de longa-metragem já lançado?',
+    'Branca de Neve e os Sete Anões'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual música ganhou o Oscar de Melhor Canção Original por sua inclusão no filme Titanic?',
+    'My Heart Will Go On de Celine Dion'
+)
+
+criaCartao(
+    'Filmes',
+    'Quem interpreta o personagem Liz Wilson, o interesse amoroso de Jon Arbuckle, no filme Garfield?',
+    'Jennifer Love Hewitt'
+)
+
+criaCartao(
+    'Filmes',
+    'Qual personagem é interpretado pelo ator Liam Hemsworth na série de filmes The Hunger Games?',
+    ' Gale Hawthorne'
+)
